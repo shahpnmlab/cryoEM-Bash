942db0c376c4122db76e122f734365f1b2fb7212
Pranav Shah <34034134+shahpnmlab@users.noreply.github.com>
2019-02-05 01:26:14 -0500

    Update and rename count_mics to count_mics.sh

--- count_mics.sh
+++ count_mics.sh
@@ -6,3 +6,4 @@
 
 awk '{print $7}' path/to/run_data.star > foo
 awk -F= '/MotionCorr/ {count[$1]++} END {for (i in count) print i, count[i]}' foo | sort -n | wc -l
+sed -i '1i _MicrographName #1'\\n'_NumberOfParticles #2' foo
